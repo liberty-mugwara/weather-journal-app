97d5c62817a7c74b37a97997f324483d208f81e3
Liberty Mugwara <libertymugwara@live.com>
2021-05-16 21:09:50 +0200

    verson 1

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 // No need to add the body-parser package I am using express > 4.16.0
 
 const app = express();
-app.use(express.static("website"));
+app.use(express.static(path.join(__dirname, "/website")));
 app.use(cors());
 
 // Please don't punish me here
@@ -12,5 +13,21 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+// API endpoint
+const projectData = [];
+
+// Router
+app.get("/weather", (req, res) => {
+  res.json(projectData);
+});
+
+app.post("/weather", addWeather);
+
+function addWeather(req, res) {
+  projectData.push(req.body);
+  res.end();
+}
+
+// starting server
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`App is running at port: ${port}`));
